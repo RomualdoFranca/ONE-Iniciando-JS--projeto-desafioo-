ae8a31245de6af0f81bbcfe64fed62ed0ca09232
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-01-18 17:21:20 -0300

    cira os alertas do jogo

---
+++ Desafios/Numero_secreto_com_pontuacao/app.js
@@ -0,0 +1,17 @@
+/**
+ * Criar as variaveis
+ * nome; pontosInicias;
+ * 
+ * Criar alertas
+ * Bem vindo;
+ * Qual o seu nome? 
+ * Regras do jogo
+ * 
+ * Criar estrutura condicional para calcular os ganhos e percas do jogo
+ * 
+ */
+
+alert('Bem vindo ao Jogo do Número Secreto');
+let usuarioNome = prompt('Qual o seu nome?');
+alert('Regras do jogo')
+alert(`Ola ${usuarioNome}. Você tem 100 ponto iniciais.\nO jogo tem 4 rodadas.\nA cada palpite correto você ganha 25 pontos.\nA cada palpite errado você perde 25 pontos.\nVocê vence o jogo se, ao final das 4 rodadas, sua pontuação for igual ou maior de 100.`)
