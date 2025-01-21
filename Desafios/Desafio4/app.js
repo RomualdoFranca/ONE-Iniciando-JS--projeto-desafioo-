4f69fbf5fd6a1fb2b03f3fbe9868a1908e831af0
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-01-20 22:54:11 -0300

    cria dois programas de soma e subtracao

--- Desafios/Desafio4/app.js
+++ Desafios/Desafio4/app.js
@@ -42,3 +42,17 @@ alert(nomeAlert);
 //Utilize o prompt e faça a seguinte pergunta: Qual a linguagem de programação que você mais gosta?. Em seguida, armazene a resposta em uma variável e mostre no console do navegador.
 let linguagemFavorita = prompt("Qual a linguagem de programação que você mais gosta?");
 console.log(linguagemFavorita);
+
+//Crie uma variável chamada "valor1" e outra chamada "valor2", atribuindo a elas valores numéricos de sua escolha. Em seguida, realize a soma desses dois valores e armazene o resultado em uma terceira variável chamada "resultado". Utilize o console.log para mostrar a mensagem "A soma de [valor1] e [valor2] é igual a [resultado]." no console.
+let valor1 = 5;
+let valor2 = 3;
+let resultado = valor1 + valor2;
+console.log(`A soma de ${valor1} + ${valor2} = ${resultado}`);
+
+//Crie uma variável chamada "valor1" e outra chamada "valor2", atribuindo a elas valores numéricos de sua escolha. Em seguida, realize a subtração desses dois valores e armazene o resultado em uma terceira variável chamada "resultado". Utilize o console.log para mostrar a mensagem "A diferença entre [valor1] e [valor2] é igual a [resultado]." no console.
+let valorSubtracao1 = 5;
+let valorSubtracao2 = 3;
+let resultadoSubtracao = valorSubtracao1 - valorSubtracao2;
+console.log(`A diferença de ${valorSubtracao1} - ${valorSubtracao2} = ${resultadoSubtracao}`);
+
+//Peça ao usuário para inserir sua idade com prompt. Com base na idade inserida, utilize um if para verificar se a pessoa é maior ou menor de idade, exibindo uma mensagem apropriada no console.
